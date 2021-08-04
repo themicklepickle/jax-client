1941130b167772de695f813be2c5c882d206c884
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-08-04 17:42:11 -0600

    Fullscreen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import { FastfoodRounded, LocalDrinkRounded } from "@material-ui/icons";
 import "./App.css";
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
 import MainButton from "./components/MainButton";
+import { Button } from "@material-ui/core";
 
 const App = () => {
+  const handle = useFullScreenHandle();
+
   return (
     <div className="wrapper">
-      <div className="icon">
-        <MainButton
-          resetTimes={[
-            { hour: 2, minutes: 0 },
-            { hour: 14, minutes: 0 },
-          ]}
-        >
-          <FastfoodRounded />
-        </MainButton>
-      </div>
-      <div className="icon">
-        <MainButton resetTimes={[{ hour: 2, minutes: 0 }]}>
-          <LocalDrinkRounded />
-        </MainButton>
-      </div>
+      <Button onClick={handle.enter}>Enter fullscreen</Button>
+
+      <FullScreen handle={handle}>
+        <div className="icon">
+          <MainButton
+            resetTimes={[
+              { hour: 2, minutes: 0 },
+              { hour: 14, minutes: 0 },
+            ]}
+          >
+            <FastfoodRounded />
+          </MainButton>
+        </div>
+        <div className="icon">
+          <MainButton resetTimes={[{ hour: 2, minutes: 0 }]}>
+            <LocalDrinkRounded />
+          </MainButton>
+        </div>
+      </FullScreen>
     </div>
   );
 };
