272d5607aaf654e02e58b8352a175eeeb20913f7
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-08-04 22:31:20 -0600

    New icons + walk button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { FastfoodRounded, LocalDrinkRounded } from "@material-ui/icons";
-import "./App.css";
 import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
+import "./App.css";
+import { Button, Grid } from "@material-ui/core";
+
+import { DogFood, DogWalk, Water } from "./svgs";
+
 import MainButton from "./components/MainButton";
-import { Button } from "@material-ui/core";
 
 const App = () => {
   const handle = useFullScreenHandle();
@@ -13,21 +15,29 @@ const App = () => {
       <Button onClick={handle.enter}>Enter fullscreen</Button>
 
       <FullScreen handle={handle}>
-        <div className="icon">
+        <Grid
+          container
+          direction="column"
+          justify="space-evenly"
+          style={{ height: "100%" }}
+        >
           <MainButton
             resetTimes={[
               { hour: 2, minutes: 0 },
               { hour: 14, minutes: 0 },
             ]}
           >
-            <FastfoodRounded />
+            <DogFood />
           </MainButton>
-        </div>
-        <div className="icon">
+
           <MainButton resetTimes={[{ hour: 2, minutes: 0 }]}>
-            <LocalDrinkRounded />
+            <Water />
           </MainButton>
-        </div>
+
+          <MainButton resetTimes={[{ hour: 2, minutes: 0 }]}>
+            <DogWalk />
+          </MainButton>
+        </Grid>
       </FullScreen>
     </div>
   );
---
+++ src/svgs/index.js
@@ -0,0 +1,3 @@
+export { ReactComponent as DogFood } from "./dog-food.svg";
+export { ReactComponent as DogWalk } from "./dog-walk.svg";
+export { ReactComponent as Water } from "./water.svg";
